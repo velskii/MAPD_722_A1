f14ed45546a3a1cec447d3b364abd8bfe46d7347
velskii <posixie@gmail.com>
2022-02-06 16:19:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MAPD_722_A1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
